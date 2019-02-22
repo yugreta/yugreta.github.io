a2f22455a22f4a06814c74709b13db597e875ebd
Greta Yu <gretayu00@gmail.com>
2019-02-22 00:51:17 -0800

    create footer and tab

---
+++ js/index.js
@@ -0,0 +1,6 @@
+function about(){
+  var education=document.getElementById("contents");
+  education.style.display="none";
+
+  document.getElementById("me").style.display="block";
+}//get other div to appear
